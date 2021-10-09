31e9dc4736d4c7941a6aed3ccecc63eb3909c204
amaqsood7 <77305913+amaqsood7@users.noreply.github.com>
2021-10-08 21:48:29 -0400

    Update lab3c.py

--- lab3c.py
+++ lab3c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Lab 3 Inv 2 function operate '''
-# Author ID: [seneca_id]
+# Author ID: amaqsood7
 
 def operate(number1, number2, operator):
     # Place logic in this function
