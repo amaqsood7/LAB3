6e03366718d777d1b71de6f5b67bb37045757472
amaqsood7 <77305913+amaqsood7@users.noreply.github.com>
2021-10-08 21:48:06 -0400

    Update lab3b.py

--- lab3b.py
+++ lab3b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Lab 3 Part 1 script - functions'''
-# Author ID: [seneca_id] 
+# Author ID: amaqsood7 
 
 def sum_numbers(number1, number2):
     # Make this function add number1 and number2 and return the value
